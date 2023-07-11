9eba05253fd774e7fe5c3a34829941d559fb64b7
KevinSucapuca <ksucapuca007@gmail.com>
2023-07-10 21:29:35 -0500

    update settings

--- CalcularNota/settings.py
+++ CalcularNota/settings.py
@@ -74,13 +74,12 @@ WSGI_APPLICATION = 'CalcularNota.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+#DATABASES = {
+#   'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': ':memory:',
-    }
-}
+# }
+#}
 
 
 # Password validation
